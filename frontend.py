496c922572b4c016070610e20e8587a7bb2e1e57
alireza <alirezawmds1379@gmail.com>
2022-05-14 14:27:55 -0700

    frontend.py changed

--- frontend.py
+++ frontend.py
@@ -3,7 +3,7 @@ import backend
 from tkinter import messagebox
 
 window = Tk()
-window.title("library appppp for alireza")
+window.title("library app for alireza")
 window.resizable(width=False,height=False)
 window.configure(bg='grey')
 
@@ -124,17 +124,10 @@ button2.grid(row=3,column=3,pady=10)
 button3 = Button(window,text="Add book",width=12,bg='orange',command=add_command)
 button3.grid(row=4,column=3,pady=10)
 
-<<<<<<< HEAD
-button4 = Button(window,text="Update Selected plz",bg='orange',width=12,command=lambda:update_command())
-button4.grid(row=5,column=3,pady=10)
-
-button5 = Button(window,text="Delete Selected plz",width=12,bg='orange',command=lambda:delete_command())
-=======
 button4 = Button(window,text="Update Selected please",bg='orange',width=12,command=lambda:update_command())
 button4.grid(row=5,column=3,pady=10)
 
 button5 = Button(window,text="Delete Selected please",width=12,bg='orange',command=lambda:delete_command())
->>>>>>> develop
 button5.grid(row=6,column=3,pady=10)
 
 button5 = Button(window,text="Close",width=12,bg='orange',command=lambda:close_command())
